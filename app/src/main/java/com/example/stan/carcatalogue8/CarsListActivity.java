c168a2df3da2b1edc8d9a925b9f14e30f358d245
Slavi <slavi_14@abv.bg>
2018-09-09 14:53:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stan/carcatalogue8/CarsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.stan.carcatalogue8;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarsListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cars_list);
+    }
+}
